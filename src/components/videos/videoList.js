59b1fbd07ebe9f0177dca333f9bd6ef19ec886d5
Harshalini <harshalinipandhare29@gmail.com>
2022-04-24 19:17:08 +0530

    suggestions by peers

--- src/components/videos/videoList.js
+++ src/components/videos/videoList.js
@@ -3,20 +3,20 @@ export const VideoList = () => {
     const { video } = useVideo();
     return (
         <div className="videos-grid">
-            {video.map((mp4) => (
+            {video.map(({_id, title, creator, views}) => (
             <div class="ui-component card card-with-badge">
                 <div class="card-image">
-                    <img src={`https://img.youtube.com/vi/${mp4._id}/hqdefault.jpg`} />
+                    <img src={`https://img.youtube.com/vi/${_id}/hqdefault.jpg`} />
                 </div>
                 <div class="top-badge">
                     <i class="fas fa-clock watch-later-btn"></i>
                     <i class="far fa-stream playlist-btn"></i>
                 </div>
                 <div class="card-text">
-                    <span className="card-title">{mp4.title}</span>
-                    <p className="creator-name">{mp4.creator}</p>
+                    <span className="card-title">{title}</span>
+                    <p className="creator-name">{creator}</p>
                     <div class="price video-stats">
-                        <p style={{color: "var(--white)"}}>Views: {mp4.views}</p>
+                        <p style={{color: "var(--white)"}}>Views: {views}</p>
                         <i class="fa-solid fa-thumbs-up like-btn"></i>
                     </div>
                 </div>
